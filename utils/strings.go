764e3a972cee0d41ea55d7405a340fb3994b7bca
Opeth001 <sabriopeth@live.fr>
2020-12-24 15:18:42 +0100

    test print modif

--- utils/strings.go
+++ utils/strings.go
@@ -1,9 +1,13 @@
 package utils
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 //ReplaceKeys formats a string using key value pairs
 func ReplaceKeys(s string, keyValues map[string]string) string {
+	fmt.Println("modif")
 	var newString = s
 	for key, value := range keyValues {
 		newString = strings.Replace(newString, "{"+key+"}", value, 1)
