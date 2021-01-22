545485a8957b5bbb38d0d1b6a44ed0740c8d0423
Opeth001 <sabriopeth@live.fr>
2021-01-22 09:08:16 +0100

    Distributed Counters Added

---
+++ firebase/distributedcounters/distributedcounters.go
@@ -0,0 +1,144 @@
+package distributedcounters
+
+import (
+"cloud.google.com/go/firestore"
+"context"
+"errors"
+"fmt"
+"math/rand"
+"strconv"
+"time"
+)
+
+
+const(
+	documentID ShardField = "did" 			//This field is used to Track/Order Shards by their Parent Document for roll-up Process
+	lastRollUpUpdate ShardField= "lru"		// This Field is used to Track Last roll-up Updates to skip un-updated Shards
+)
+
+var (
+	internalKeys = []ShardField {documentID,lastRollUpUpdate}
+)
+
+func checkInternalFieldsUsage(field ShardField){
+	for _,key := range internalKeys{
+		if key == field{
+			panic(fmt.Sprintf("DistributedCounters Package do not Allow the usage of internal Keys: %v",internalKeys) )
+		}
+	}
+}
+
+var NoShardFieldSpecified = errors.New("no shard fields specified")
+
+type ShardField string
+
+type DistributedShard map[ShardField] interface{}
+
+// distributedCounter is a collection of documents (shards)
+// to realize counter with high frequency.
+//This Struct will be created by every Incremental Section (Videos Likes, Comments Likes ..)
+type distributedCounter struct {
+	ShardName string
+	NumShards int
+	shardFields DistributedShard
+}
+
+//CreateDistributedCounter returns a CreateDistributedCounter to manage Shards
+func CreateDistributedCounter (shardName string, numShards int) distributedCounter {
+	return distributedCounter{
+		ShardName:   shardName,
+		NumShards:   numShards,
+		shardFields: make(map[ShardField] interface{}),
+	}
+}
+
+
+//AddFieldForUpdate Adds a Shard.Field for updated
+func (c *distributedCounter) AddFieldForUpdate(field ShardField, value interface{} ){
+	checkInternalFieldsUsage(field)
+	c.shardFields[field] = value
+}
+
+//Increments a Shard.Field for updated.
+//Note! The Shard
+//The supported values are:
+//   int, int8, int16, int32, int64
+//   uint8, uint16, uint32
+//   float32, float64
+func (c *distributedCounter) IncrementField(field ShardField, value interface{} ){
+	checkInternalFieldsUsage(field)
+	c.shardFields[field] = firestore.Increment(value)
+}
+
+
+
+// CreateShards creates a given number of shards as sub-collection of the specified document.
+//(This operation need to be done once per Document or it will reinitialize all shards Data )
+func (c *distributedCounter) CreateShards(ctx context.Context, docRef *firestore.DocumentRef, shardData interface{}) error {
+	colRef := docRef.Collection(c.ShardName)
+
+	// Initialize each shard with count=0
+	for num := 0; num < c.NumShards; num++ {
+		if _, err := colRef.Doc(strconv.Itoa(num)).Set(ctx, shardData); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+
+
+// UpdateCounters updates a randomly picked shard of a Document.
+//If no ShardField specified, an NoShardFieldSpecified will be returned
+func (c *distributedCounter) UpdateCounters(ctx context.Context, docRef *firestore.DocumentRef) (*firestore.WriteResult, error) {
+	updateCount := len(c.shardFields)
+	if  updateCount == 0 {
+		return nil, NoShardFieldSpecified
+	}
+	rand.Seed(time.Now().UnixNano())
+	docID := strconv.Itoa(rand.Intn(c.NumShards))
+	shardRef := docRef.Collection(c.ShardName).Doc(docID)
+
+	//preallocate the slice for performance reasons
+	updatedFields :=  make([]firestore.Update,updateCount +2)
+	index := 0
+	for key, value := range c.shardFields {
+		updatedFields[index] = firestore.Update{
+			Path:      string(key),
+			Value:     value,
+		}
+		index++
+	}
+
+	//Add DocumentID for roll-up Updates
+	updatedFields[index] = firestore.Update{
+		Path:      string(documentID),
+		Value:     docRef.ID,
+	}
+
+	//Add DocumentID for roll-up Updates
+	updatedFields[index+1] = firestore.Update{
+		Path:      string(lastRollUpUpdate),
+		Value:     time.Now(),
+	}
+
+	return shardRef.Update(ctx, updatedFields)
+}
+
+
+
+
+/*
+// UpdateCounter increments a randomly picked shard.
+func (c *distributedCounter) UpdateCounter(ctx context.Context, docRef *firestore.DocumentRef) (*firestore.WriteResult, error) {
+	rand.Seed(time.Now().UnixNano())
+
+	docID := strconv.Itoa(rand.Intn(c.NumShards))
+
+	shardRef := docRef.Collection(c.ShardName).Doc(docID)
+	return shardRef.Update(ctx, []firestore.Update{
+		{Path: "c", Value: firestore.Increment(1)},
+		{Path: "lu", Value: time.Now()},
+	})
+}
+*/
---
+++ firebase/distributedcounters/go.mod
@@ -0,0 +1,5 @@
+module github.com/sabriboughanmi/go_utils/firebase/distributedcounters
+
+go 1.13
+
+require cloud.google.com/go/firestore v1.4.0
