6ff79268086845cceb3476c59ab67c7982c15d96
Opeth001 <sabriopeth@live.fr>
2021-02-05 10:34:21 +0100

    update

--- utils/strings.go
+++ utils/strings.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"math/rand"
 	"strings"
+	"time"
 )
 
 //ReplaceKeys formats a string using key value pairs
@@ -11,4 +13,41 @@ func ReplaceKeys(s string, keyValues map[string]string) string {
 		newString = strings.Replace(newString, "{"+key+"}", value, 1)
 	}
 	return newString
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+//Returns a Random AlphaNumeric String with a given length.
+//Note! passing the same seed returns the same Random String
+func RandomStringWithSeed(length, seed int64) string {
+	return RandomstringCharsetSeed(length, seed, charset)
+}
+
+//Returns a Random String from specified Charset.
+//Note! passing the same seed returns the same Random String
+func RandomstringCharsetSeed(length, seed int64, charset string) string {
+	var seededRand = rand.New(rand.NewSource(seed))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+//Returns a Random AlphaNumeric String with a given length.
+//Note! this Function sleeps for a time.Nanosecond to return a different Value every call, if you care about Performance use  RandomStringWithSeed instead.
+func RandomString(length int) string {
+	return RandomstringCharset(length, charset)
+}
+
+//Returns a Random String from specified Charset.
+//Note! this Function sleeps for a time.Nanosecond to return a different Value every call, if you care about Performance use  RandomstringCharsetSeed instead.
+func RandomstringCharset(length int, charset string) string {
+	time.Sleep(time.Nanosecond)
+	var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
