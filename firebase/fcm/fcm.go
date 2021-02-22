88ca0c56a3ecb420622e1e6d53c2843b18778b7f
Opeth001 <sabriopeth@live.fr>
2021-02-22 16:46:46 +0100

    update

--- firebase/fcm/fcm.go
+++ firebase/fcm/fcm.go
@@ -6,8 +6,6 @@ import (
 	"firebase.google.com/go/messaging"
 )
 
-
-
 //Creates a Notification Manager
 func GetNotificationManager(app *firebase.App, ctx context.Context) (NotificationManager, error) {
 	client, err := app.Messaging(ctx)
@@ -18,34 +16,68 @@ func GetNotificationManager(app *firebase.App, ctx context.Context) (Notificatio
 }
 
 //Send a Message to Multiple Users
-func (nm *NotificationManager) SendMessageToMultipleUsers(data map[string]string, registrationTokens ...string) (*messaging.BatchResponse, error) {
+func (nm *NotificationManager) UsersSendMessage(title, body, url string, data map[string]string, registrationTokens ...string) (*messaging.BatchResponse, error) {
 	messages := &messaging.MulticastMessage{
-		Tokens:       registrationTokens,
-		Data:         data,
-		Notification: nil,
-		Android:      nil,
-		Webpush:      nil,
-		APNS:         nil,
+		Tokens: registrationTokens,
+		Data:   data,
+		Notification: &messaging.Notification{
+			Title:    title,
+			Body:     body,
+			ImageURL: url,
+		},
 	}
-
 	// Send a message to the device corresponding to the provided registration token.
 	return nm.client.SendMulticast(nm.ctx, messages)
 }
 
 //Send a Message to a single User
-func (nm *NotificationManager) SendMessage(data map[string]string, registrationToken string) (string, error) {
+func (nm *NotificationManager) UserSendMessage(title, body, url string, data map[string]string, registrationToken string) (string, error) {
 
 	// See documentation on defining a message payload.
 	message := &messaging.Message{
-		Data:         data,
-		Notification: nil,
-		Android:      nil,
-		Webpush:      nil,
-		APNS:         nil,
-		FCMOptions:   nil,
-		Token:        registrationToken,
-		Topic:        "",
-		Condition:    "",
+		Data: data,
+		Notification: &messaging.Notification{
+			Title:    title,
+			Body:     body,
+			ImageURL: url,
+		},
+		Token: registrationToken,
+	}
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	return nm.client.Send(nm.ctx, message)
+}
+
+//Send a Message to all devices for which the Condition returns True
+func (nm *NotificationManager) ConditionSendMessage(title, body, url string, data map[string]string, condition string) (string, error) {
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: data,
+		Notification: &messaging.Notification{
+			Title:    title,
+			Body:     body,
+			ImageURL: url,
+		},
+		Condition: condition,
+	}
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	return nm.client.Send(nm.ctx, message)
+}
+
+//Send a Message to all devices subscribed to the given Topic
+func (nm *NotificationManager) TopicSendMessage(title, body, url string, data map[string]string, topic string) (string, error) {
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: data,
+		Notification: &messaging.Notification{
+			Title:    title,
+			Body:     body,
+			ImageURL: url,
+		},
+		Topic: topic,
 	}
 
 	// Send a message to the device corresponding to the provided
