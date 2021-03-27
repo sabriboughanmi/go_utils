fb7d2b71baef46fdacd553c01eade7ed6a855c61
Opeth001 <sabriopeth@live.fr>
2021-03-27 14:13:27 +0100

    update

--- math/arrays/difference.go
+++ math/arrays/difference.go
@@ -3,7 +3,7 @@ package arrays
 //Difference returns the Difference between 2 arrays (A - B)
 //EG: a :[1, 2, 3, 4, 5]  - b: [2, 3, 5, 7, 11] =
 // result: [1 4].
-func Difference(a, b []uint16) (diff []uint16) {
+func Difference_Uint16(a, b []uint16) (diff []uint16) {
 	m := make(map[uint16]bool)
 
 	for _, item := range b {
@@ -18,5 +18,20 @@ func Difference(a, b []uint16) (diff []uint16) {
 	return
 }
 
+//Difference returns the Difference between 2 arrays (A - B)
+//EG: a :["1", "2", "3", "4", "5"]  - b: ["2", "3", "5", "7", "11"] =
+// result: ["1", "4"].
+func Difference_String(a, b []string) (diff []string) {
+	m := make(map[string]bool)
 
+	for _, item := range b {
+		m[item] = true
+	}
 
+	for _, item := range a {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+	return
+}
--- math/arrays/union.go
+++ math/arrays/union.go
@@ -1,8 +1,8 @@
 package arrays
 
 //Union returns the sum of 2 arrays (A + B) without duplicates
-//EG: a :[1, 2, 3, 4, 5]  + b: [2, 3, 5, 7, 11] =
-// result: [1 ,2 ,3, 4, 5, 7 ,11].
+//EG: a :["1", "2", "3", "4", "5"]  + b: ["2", "3", "5", "7", "11"] =
+// result: ["1" ,"2" ,"3", "4", "5", "7" ,"11"].
 func Union(a, b []string) []string {
 	check := make(map[string]int16)
 	d := append(a, b...)
