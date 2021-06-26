6930db33aef6d9fa775290382d949d2115bae503
Opeth001 <sabriopeth@live.fr>
2021-06-26 12:13:03 +0100

    update

--- adNetworks/admob/admob.go
+++ adNetworks/admob/admob.go
@@ -2,6 +2,7 @@ package admob
 
 import (
 	"github.com/hiyali/go-lib-ssv/admob"
+	"github.com/sabriboughanmi/go_utils/utils"
 	"net/url"
 )
 
@@ -10,7 +11,9 @@ func VerifyURL(callBackUrl *url.URL) error {
 	return admob.Verify(callBackUrl)
 }
 
-func GetParameters(requestBody string, out interface{}) error {
-
-	return nil
+//GetParameters Returns the SSVCallback model sent by Admob
+func GetParameters(requestBody string) (SSVCallback, error) {
+	var admobSSVCallback SSVCallback
+	err := utils.RequestUrlToStruct(requestBody, &admobSSVCallback, utils.JsonMapper)
+	return admobSSVCallback, err
 }
--- adNetworks/admob/go.mod
+++ adNetworks/admob/go.mod
@@ -2,4 +2,7 @@ module github.com/sabriboughanmi/go_utils/adNetworks/admob
 
 go 1.13
 
-require github.com/hiyali/go-lib-ssv v0.0.2
+require (
+	github.com/hiyali/go-lib-ssv v0.0.2
+	github.com/sabriboughanmi/go_utils/utils v0.0.0-20210626110908-8f713c20e808
+)
