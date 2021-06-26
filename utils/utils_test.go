7fa43d1b59cebc184263806ebc767cced5b74fa4
Opeth001 <sabriopeth@live.fr>
2021-06-26 12:59:05 +0100

    update

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -8,8 +8,9 @@ import (
 func TestRequestUrlToStruct(t *testing.T) {
 	var sampleUrl = "int=88&float=32.793&bool=false&string=Marsiella&array_string[]=Jobi&array_string[]=Betta&array_string[]=Lilith&array_string[]=Donetta&array_string[]=Cristabel&array_objects[0][index]=0&array_objects[0][indexString]=5&array_objects[1][index]=1&array_objects[1][indexString]=6&array_objects[2][index]=2&array_objects[2][indexString]=7&subMap[uint]=71&subMap[string]=Sabri&subMap[7ala]=true&subMap[subMap_array_int8][]=0&subMap[subMap_array_int8][]=10&subMap[subMap_array_int8][]=50&subMap[subMap_array_int8][]=60&subMap[subMap_array_int8][]=127&array_2D[0][]=0&array_2D[0][]=1&array_2D[0][]=2&array_2D[1][]=3&array_2D[1][]=4&array_2D[1][]=5&array_2D[1][]=6&array_3D[0][0][]=0&array_3D[0][0][]=1&array_3D[0][0][]=2&array_3D[0][1][]=0&array_3D[0][1][]=1&array_3D[0][1][]=2&array_3D[0][2][]=0&array_3D[0][2][]=1&array_3D[0][2][]=2&array_3D[1][0][]=0&array_3D[1][0][]=1&array_3D[1][0][]=2&array_3D[1][1][]=0&array_3D[1][1][]=1&array_3D[1][1][]=2&array_3D[1][2][]=0&array_3D[1][2][]=1&array_3D[1][2][]=2&array_3D[2][0][]=0&array_3D[2][0][]=1&array_3D[2][0][]=2&array_3D[2][1][]=0&array_3D[2][1][]=1&array_3D[2][1][]=2&array_3D[2][2][]=0&array_3D[2][2][]=1&array_3D[2][2][]=2"
 
+
 	type Object struct {
-		Index       int64  `json:"index"`
+		Index       int  `json:"index"`
 		IndexString string `json:"indexString"`
 	}
 
@@ -19,8 +20,10 @@ func TestRequestUrlToStruct(t *testing.T) {
 		The7Ala         bool   `json:"7ala"`
 		SubMapArrayInt8 []int8 `json:"subMap_array_int8"`
 	}
+
+	type ENUM int
 	type Main struct {
-		Int          int64       `json:"int"`
+		Int          ENUM       `json:"int"`
 		Float        float64     `json:"float"`
 		Bool         bool        `json:"bool"`
 		String       string      `json:"string"`
@@ -38,4 +41,6 @@ func TestRequestUrlToStruct(t *testing.T) {
 	} else {
 		fmt.Println("func RequestUrlToStruct OK!")
 	}
+
+	fmt.Println(string(UnsafeAnythingToJSON(main)))
 }
