fc39a2033860567b553bb4119421b59b81a65a7e
syrineT4U <syrine.mahjoub@t4ulabs.com>
2021-08-23 18:54:10 +0100

    moderate video added

--- ffmpeg/ffmpeg_test.go
+++ ffmpeg/ffmpeg_test.go
@@ -3,12 +3,11 @@ package ffmpeg
 import (
 	"cloud.google.com/go/storage"
 	"context"
-	"fmt"
-	"github.com/sabriboughanmi/go_utils/ffmpeg"
 	"google.golang.org/api/option"
 	"sync"
 	"testing"
 )
+
 var storageClientOnce sync.Once
 var StorageClient *storage.Client
 var ctx context.Context
@@ -16,8 +15,6 @@ var ctx context.Context
 func init() {
 	ctx = context.Background()
 }
-
-
 
 //GetStorageClient returns a Singleton *storage.Client
 func GetStorageClient() (*storage.Client, error) {
@@ -29,17 +26,19 @@ func GetStorageClient() (*storage.Client, error) {
 	})
 	return StorageClient, err
 }
-func TestModerateVideo(t *testing.T){
+func TestModerateVideo(t *testing.T) {
 	storageClient, err := GetStorageClient()
 	if err != nil {
 		t.Errorf("Error - %v", err)
 	}
 	var temporaryStorageObject temporaryStorageObjectRef
-	temporaryStorageObject.Bucket="gs://tested4you-dev.appspot.com/"
-	temporaryStorageObject.Client=storageClient
-	vid, err := ffmpeg.LoadVideo("https://www.youtube.com/watch?v=w01V5FI03MQ")
-	if err != nil {
-			fmt.Printf("error")
-	}
-	//vid.
+	temporaryStorageObject.Bucket = "gs://tested4you-dev.appspot.com/"
+	temporaryStorageObject.Client = storageClient
+	/*
+		vid, err := ffmpeg.LoadVideo("https://www.youtube.com/watch?v=w01V5FI03MQ")
+		if err != nil {
+				fmt.Printf("error")
+		}
+	*/
+
 }
--- ffmpeg/go.mod
+++ ffmpeg/go.mod
@@ -2,4 +2,11 @@ module github.com/sabriboughanmi/go_utils/ffmpeg
 
 go 1.13
 
-require github.com/sabriboughanmi/go_utils/os v0.0.0-20210511145304-e5e549d9ad92
+require (
+	cloud.google.com/go/storage v1.16.0
+	cloud.google.com/go/vision v0.1.0
+	github.com/sabriboughanmi/go_utils/firebase/storage v0.0.0-20210818140617-cd03a0b687f6
+	github.com/sabriboughanmi/go_utils/os v0.0.0-20210511145304-e5e549d9ad92
+	google.golang.org/api v0.54.0
+	google.golang.org/protobuf v1.27.1
+)
