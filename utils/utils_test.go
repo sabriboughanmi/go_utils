e1f83873b364efdd8c0a621d24715b8196d17235
Opeth001 <sabriopeth@live.fr>
2021-09-07 00:58:24 +0100

    Rate Limiter API Added

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,9 +2,12 @@ package utils
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
+/*
 func TestRequestUrlToStruct(t *testing.T) {
 	var sampleUrl = "int=88&float=32.793&bool=false&string=Marsiella&array_string[]=Jobi&array_string[]=Betta&array_string[]=Lilith&array_string[]=Donetta&array_string[]=Cristabel&array_objects[0][index]=0&array_objects[0][indexString]=5&array_objects[1][index]=1&array_objects[1][indexString]=6&array_objects[2][index]=2&array_objects[2][indexString]=7&subMap[uint]=71&subMap[string]=Sabri&subMap[7ala]=true&subMap[subMap_array_int8][]=0&subMap[subMap_array_int8][]=10&subMap[subMap_array_int8][]=50&subMap[subMap_array_int8][]=60&subMap[subMap_array_int8][]=127&array_2D[0][]=0&array_2D[0][]=1&array_2D[0][]=2&array_2D[1][]=3&array_2D[1][]=4&array_2D[1][]=5&array_2D[1][]=6&array_3D[0][0][]=0&array_3D[0][0][]=1&array_3D[0][0][]=2&array_3D[0][1][]=0&array_3D[0][1][]=1&array_3D[0][1][]=2&array_3D[0][2][]=0&array_3D[0][2][]=1&array_3D[0][2][]=2&array_3D[1][0][]=0&array_3D[1][0][]=1&array_3D[1][0][]=2&array_3D[1][1][]=0&array_3D[1][1][]=1&array_3D[1][1][]=2&array_3D[1][2][]=0&array_3D[1][2][]=1&array_3D[1][2][]=2&array_3D[2][0][]=0&array_3D[2][0][]=1&array_3D[2][0][]=2&array_3D[2][1][]=0&array_3D[2][1][]=1&array_3D[2][1][]=2&array_3D[2][2][]=0&array_3D[2][2][]=1&array_3D[2][2][]=2"
 
@@ -44,4 +47,23 @@ func TestRequestUrlToStruct(t *testing.T) {
 
 	//fmt.Println(string(UnsafeAnythingToJSON(main)))
 }
+*/
 
+func TestRateLimiter(t *testing.T) {
+	var rateLimiter = CreateLimiter(time.Millisecond*200, 4)
+
+	rateLimiter.Start()
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(waitGroup *sync.WaitGroup) {
+			defer waitGroup.Done()
+			rateLimiter.Wait()
+			fmt.Println(time.Now())
+			//Code here
+		}(&wg)
+	}
+	wg.Wait()
+	rateLimiter.Stop()
+
+}
