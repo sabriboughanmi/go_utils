3ef57ce5f9b6080bbb398c83138c459dbd4ab8bc
Sabri <sabri.boughanmi@t4ulabs.com>
2022-03-19 13:41:57 +0100

    added noRewardsPossibility to the PeekRandomElement function util

--- utils/random.go
+++ utils/random.go
@@ -11,13 +11,14 @@ func RandFloat32(min, max float32, seed int64) float32 {
 }
 
 //PeekRandomElement peeks a random element from a set of win rates percentages.
-func PeekRandomElement(percentages []float32) int {
+//	- noRewardsPossibility : True and percentages sum is less than 100% it sets it to 100%.
+//	which means it gives the possibility to return -1 index. (No Element Selected)
+func PeekRandomElement(percentages []float32, noRewardsPossibility bool) int {
 
 	var totalPoints float32 = 0
 
 	//Sum percentages to make a random choice
 	for _, percentage := range percentages {
-
 		totalPoints += percentage
 	}
 
@@ -26,14 +27,19 @@ func PeekRandomElement(percentages []float32) int {
 		return 0
 	}
 
+	var randomIndex = 0
+	//Make no selected element possible
+	if totalPoints < 100 && noRewardsPossibility {
+		totalPoints = 100
+		randomIndex = -1
+	}
+
 	//ensure that rand has a new seed value
 	time.Sleep(time.Microsecond)
 	randomRewardValue := RandFloat32(0, totalPoints, time.Now().UnixNano())
 
 	// Pick a random Card
 	var accumulatedPoints float32 = 0
-
-	var randomIndex = 0
 
 	//Iterate over Cards
 	for i, percentage := range percentages {
